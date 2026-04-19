d03aa79eeac00850b97a67a1478ccf38231e1229
Manukashyap.U.V <manukashyap.u.v@gmail.com>
2026-04-19 22:16:53 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() < 2) return false;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (isOpenParantheses(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (!isValidParantheses(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty() ? true: false;
+    }
+
+    private boolean isOpenParantheses(char c) {
+        return (c == '{' || c == '[' || c == '(');
+    }
+
+    private boolean isValidParantheses(char open, char close) {
+        return ((open == '(' && close == ')') || (open == '[' && close == ']')
+            || (open == '{' && close == '}'));
+    }
+}
